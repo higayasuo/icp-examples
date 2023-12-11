e1b4cf1bea4cac90492c842bfb7de01ed8c85a94
Yasuo Higa <higayasuo@gmail.com>
2023-12-11 00:58:57 +0000

    Add http_incoming

---
+++ http_incoming/src/http_incoming_backend/src/http.rs
@@ -0,0 +1,23 @@
+use candid::CandidType;
+use serde::{Deserialize, Serialize};
+
+// #[derive(Debug, CandidType, Serialize, Deserialize, Clone)]
+// pub struct HeaderField(String, String);
+
+#[derive(Debug, CandidType, Deserialize, Clone)]
+pub struct HttpRequest {
+    pub method: String,
+    pub url: String,
+    pub headers: Vec<(String, String)>,
+    //#[serde(with = "serde_bytes")]
+    pub body: Vec<u8>,
+}
+
+#[derive(CandidType, Serialize)]
+pub struct HttpResponse {
+    pub status_code: u16,
+    pub headers: Vec<(String, String)>,
+    //#[serde(with = "serde_bytes")]
+    pub body: Vec<u8>,
+    //pub upgrade: Option<bool>,
+}
---
+++ http_incoming/src/http_incoming_backend/src/lib.rs
@@ -0,0 +1,40 @@
+use base64::{engine::general_purpose, Engine as _};
+use ic_cdk::{export_candid, query};
+
+mod http;
+use http::{HttpRequest, HttpResponse};
+
+const FAVICON: &str = "AAABAAEAEBAAAAEAIABoBAAAFgAAACgAAAAQAAAAIAAAAAEAIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A////AP///wD///8A//8AAP//AAD//wAA//8AAP//AAD//wAA//8AAP//AAD//wAA//8AAP//AAD//wAA//8AAP//AAD//wAA//8AAA==";
+
+#[query]
+async fn http_request(req: HttpRequest) -> HttpResponse {
+    ic_cdk::println!("{:?}", req);
+    if req.url == *"/favicon.ico" {
+        return favicon_response();
+    }
+
+    HttpResponse {
+        status_code: 200_u16,
+        headers: vec![("Content-Type".to_string(), "text/plain".to_string())],
+        body: "Hello".to_string().into_bytes(),
+    }
+}
+
+fn favicon_response() -> HttpResponse {
+    let body = general_purpose::STANDARD.decode(FAVICON).unwrap();
+    ic_cdk::println!("favicon_response: {}", body.len());
+
+    HttpResponse {
+        status_code: 200_u16,
+        headers: vec![
+            (
+                "Content-Type".to_string(),
+                "image/vnd.microsoft.icon".to_string(),
+            ),
+            ("Content-Length".to_string(), body.len().to_string()),
+        ],
+        body,
+    }
+}
+
+export_candid!();
