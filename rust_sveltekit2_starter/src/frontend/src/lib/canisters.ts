1ddec6ee2d0651cd8e94b1a55625fba4f65643db
Yasuo Higa <higayasuo@gmail.com>
2024-12-16 08:51:25 +0900

    Add expo-starter project

--- rust_sveltekit2_starter/src/frontend/src/lib/canisters.ts
+++
@@ -1,6 +0,0 @@
-import { createActor, canisterId } from '../../../declarations/backend';
-
-export const host =
-	process.env.DFX_NETWORK == 'local' ? `http://localhost:4943` : `https://${canisterId}.ic0.app`;
-
-export const backend = createActor(canisterId, { agentOptions: { host } });
